9f4b7d1f2ebc3bd850ad8d6e37975d982d0cb97d
sardinhas12 <patriciasardinharocha@gmail.com>
2022-08-16 23:01:10 +0000

    first commit

---
+++ src/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+from flask import Flask
+from flask import request
+import json
+
+app = Flask(__name__)
+
+todos = [ { "label": "My first task", "done": False } ]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+      request_body = request.data
+      todos.append(request.get_json(force=True))
+      return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete:", position)
+    del todos[position]
+    return jsonify(todos)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
